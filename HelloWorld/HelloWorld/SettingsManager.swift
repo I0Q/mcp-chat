f2641fccfdda1ebb225f4e79d91927ab4e5e317f
Acacio <o@acac.io>
2025-10-26 17:34:24 -0400

    Add default values for Home Assistant MCP server configuration

--- HelloWorld/HelloWorld/SettingsManager.swift
+++ HelloWorld/HelloWorld/SettingsManager.swift
@@ -50,9 +50,22 @@ class SettingsManager: ObservableObject {
     private init() {
         self.serverURL = UserDefaults.standard.string(forKey: "serverURL") ?? "http://192.168.1.232:1234"
         self.mcpEnabled = UserDefaults.standard.bool(forKey: "mcpEnabled")
-        self.mcpServerName = UserDefaults.standard.string(forKey: "mcpServerName") ?? ""
-        self.mcpSSEURL = UserDefaults.standard.string(forKey: "mcpSSEURL") ?? ""
-        self.mcpAccessToken = UserDefaults.standard.string(forKey: "mcpAccessToken") ?? ""
+        
+        // Set default MCP values if not already configured
+        self.mcpServerName = UserDefaults.standard.string(forKey: "mcpServerName") ?? "Home Assistant"
+        self.mcpSSEURL = UserDefaults.standard.string(forKey: "mcpSSEURL") ?? "http://homeassistant:8123/mcp_server/sse"
+        self.mcpAccessToken = UserDefaults.standard.string(forKey: "mcpAccessToken") ?? "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIwZjA1M2JjZWRjNzk0NTlmOGZjMTQ3ZWYwZDVhZWM4MCIsImlhdCI6MTc2MTQ5NDQwNiwiZXhwIjoyMDc2ODU0NDA2fQ.PSwfpbey4BXe2TmScH5PxVMhgOVsjVqU8sdx5twQjZU"
+        
+        // Save defaults if they don't exist
+        if UserDefaults.standard.string(forKey: "mcpServerName") == nil {
+            UserDefaults.standard.set("Home Assistant", forKey: "mcpServerName")
+        }
+        if UserDefaults.standard.string(forKey: "mcpSSEURL") == nil {
+            UserDefaults.standard.set("http://homeassistant:8123/mcp_server/sse", forKey: "mcpSSEURL")
+        }
+        if UserDefaults.standard.string(forKey: "mcpAccessToken") == nil {
+            UserDefaults.standard.set("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIwZjA1M2JjZWRjNzk0NTlmOGZjMTQ3ZWYwZDVhZWM4MCIsImlhdCI6MTc2MTQ5NDQwNiwiZXhwIjoyMDc2ODU0NDA2fQ.PSwfpbey4BXe2TmScH5PxVMhgOVsjVqU8sdx5twQjZU", forKey: "mcpAccessToken")
+        }
         
         // Migrate old model names to new format
         if let savedModel = UserDefaults.standard.string(forKey: "selectedModel") {
