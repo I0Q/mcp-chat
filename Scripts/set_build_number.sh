e513e7efbcfef094ed17301fba18fec91217c161
Acacio <o@acac.io>
2025-10-28 11:27:52 -0400

    Add more debugging to build script
    - Show current working directory
    - Display SRCROOT environment variable
    - This will help identify if script is running in Xcode build environment

--- Scripts/set_build_number.sh
+++ Scripts/set_build_number.sh
@@ -8,12 +8,14 @@ target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
 dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
 
 echo "=== Build Number Script Debug ==="
+echo "Script running from: $(pwd)"
 echo "Git version: $git_release_version"
 echo "Build number: $number_of_commits"
 echo "Target plist: $target_plist"
 echo "DSYM plist: $dsym_plist"
 echo "TARGET_BUILD_DIR: $TARGET_BUILD_DIR"
 echo "INFOPLIST_PATH: $INFOPLIST_PATH"
+echo "SRCROOT: $SRCROOT"
 
 for plist in "$target_plist" "$dsym_plist"; do
   if [ -f "$plist" ]; then
